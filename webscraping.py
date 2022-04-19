8fc17e3f47ed35169db750c92d51fdae4067800f
Davi-Ga <davigaldinoky@gmail.com>
2022-04-19 11:13:16 -0300

    Montagem do scrapy

--- webscraping.py
+++ webscraping.py
@@ -1,4 +1,9 @@
 import scrapy
 
 class Teia(scrapy.Spider):
-    nome= 'web'
+    nome= 'QuotesSpider'
+    start_urls = ['http://quotes.toscrape.com/']
+    
+    def parse(self,response):
+        quotes = response.xpath('*//div[@class="quote"]')
+        for i in quotes:
